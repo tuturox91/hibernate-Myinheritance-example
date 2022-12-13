cca9e802228d484fc1b1d69124fdf3faecc21b92
Igor Bukur <tuturox91@gmail.com>
2022-12-13 16:09:30 +0200

    Need Fixes

--- src/main/java/core/basesyntax/dao/figure/FigureDaoImpl.java
+++ src/main/java/core/basesyntax/dao/figure/FigureDaoImpl.java
@@ -3,7 +3,12 @@ package core.basesyntax.dao.figure;
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.figure.Figure;
 import java.util.List;
+
+import core.basesyntax.util.HibernateUtil;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class FigureDaoImpl<T extends Figure> extends AbstractDao implements FigureDao<T> {
     public FigureDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +17,35 @@ public class FigureDaoImpl<T extends Figure> extends AbstractDao implements Figu
 
     @Override
     public T save(T figure) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(figure);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Cant save figure: " + figure, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return figure;
     }
 
     @Override
     public List<T> findByColor(String color, Class<T> clazz) {
-        return null;
+        try(Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<T> query = session.createQuery("FROM " + clazz.getSimpleName()
+                    + " WHERE color = :color", clazz);
+            query.setParameter("color", color);
+            return  query.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get figures by color: " + color, e);
+        }
     }
 }
--- src/main/java/core/basesyntax/dao/ma/MentorDaoImpl.java
+++ src/main/java/core/basesyntax/dao/ma/MentorDaoImpl.java
@@ -2,7 +2,11 @@ package core.basesyntax.dao.ma;
 
 import core.basesyntax.model.ma.Mentor;
 import java.util.List;
+
+import core.basesyntax.util.HibernateUtil;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 
 public class MentorDaoImpl extends PersonDaoImpl implements MentorDao {
     public MentorDaoImpl(SessionFactory sessionFactory) {
@@ -11,6 +15,13 @@ public class MentorDaoImpl extends PersonDaoImpl implements MentorDao {
 
     @Override
     public List<Mentor> findByAgeGreaterThan(int age) {
-        return null;
+        try(Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<Mentor> query = session.createQuery(
+                    "FROM Mentor m where m.age = :age", Mentor.class);
+            query.setParameter("age", age);
+            return query.getResultList();
+        } catch (Exception e) {
+            throw  new RuntimeException("Can't get mentor from DB", e);
+        }
     }
 }
